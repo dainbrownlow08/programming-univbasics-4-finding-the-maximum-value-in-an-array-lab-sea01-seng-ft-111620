ddd2502a4df49c0a936dab2e20fe033146d986ca
Dain Brownlow <dain.brownlow@outlook.com>
2020-10-30 04:25:43 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,11 +1,5 @@
 def find_max_value(array)
   max = 0
-  for i in array do
-    if array[i] != nil && array[i] > max
-      max = array[i]
-    else
-      max = max
-    end
-  end
-  return max
+  
+
 end
